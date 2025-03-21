d5c4c5f603945ca2343187a39261fd3ea4ea0cc0
ooosaretin <42819041+ooosaretin@users.noreply.github.com>
2025-03-21 10:46:09 -0500

    Update GoogleSearchSelenium.js

--- GoogleSearchTest/GoogleSearchSelenium.js
+++ GoogleSearchTest/GoogleSearchSelenium.js
@@ -2,14 +2,9 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
 
 async function google_search_test() {
   let driver = await new Builder().forBrowser("chrome").build();
-  try {
-    await driver.get("https://ww.google.com");
-    await driver.findElement(By.name('q')).sendKeys("Subaru Outback", Key.RETURN);
-    await driver.wait(until.titleIs('Subaru Outback - Google Search'), 2000);
-  } finally {
-    await driver.quit();
-  }
-
+  await driver.get("https://ww.google.com");
+  await driver.findElement(By.name('q')).sendKeys("Subaru Outback", Key.RETURN);
+  await driver.wait(until.titleIs('Subaru Outback - Google Search'), 2000);
 }
 
 google_search_test();
